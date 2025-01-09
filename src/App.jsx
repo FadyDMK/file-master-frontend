82f26072412d7576e62dd36670805450a5e3a819
Fady Damak <32818953+FadyDMK@users.noreply.github.com>
2025-01-09 16:14:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Signup />
+      
+    </>
+  );
+}
+
+export default App;
